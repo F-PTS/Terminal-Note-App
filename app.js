7d14bf47b746f04005d3e7af02ca1cf78342050f
Filip Pitus <80021800+F-PTS@users.noreply.github.com>
2022-07-21 20:06:16 +0200

    setting up terminal commands with yargs

--- app.js
+++ app.js
@@ -1,11 +1,58 @@
 const validator = require('validator');
 const chalk = require('chalk');
+const yargs = require('yargs');
 
 const getNotes = require('./notes.js');
 
-// get the input value from terminal...
-const command = process.argv[2];
+// get the terminal commands with yargs...
 
-// ...and manage them
-if(command === 'add') console.log('Adding note!');
-if(command === 'remove') console.log('Removing note!');
+// Create: add
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler() {
+        console.log('Adding new note!');
+    }
+});
+
+// Read: read
+yargs.command({
+    command: 'read',
+    describe: 'read selected note',
+    handler() {
+        console.log('the note:   {{ note here lol }}');
+    }
+});
+
+yargs.command({
+    command: 'list',
+    describe: 'list all notes',
+    handler() {
+        console.log('your notes:  {{ notes here :P }} ')
+    }
+})
+
+// Update: edit
+yargs.command({
+    command: 'edit',
+    describe: 'edit selected note',
+    handler() {
+        console.log('well I gotta think how to do it the right way');
+    }
+});
+
+// Delete: remove
+yargs.command({
+    command: 'remove',
+    describe: 'remove selected note',
+    handler() {
+        console.log('remove selected note')
+    }
+})
+
+
+//  ...and manage them
+// if(command === 'add') console.log('Adding note!');
+// if(command === 'remove') console.log('Removing note!');
+
+console.log(yargs.argv)
