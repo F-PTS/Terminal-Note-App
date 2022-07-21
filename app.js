c0e973938d11df938aa6b58be3ab8c011fa8ddb9
Filip Pitus <80021800+F-PTS@users.noreply.github.com>
2022-07-21 19:44:08 +0200

    process.argv inputs enabled

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
+const validator = require('validator');
+const chalk = require('chalk');
+
 const getNotes = require('./notes.js');
 
-const msg = getNotes();
+// get the input value from terminal...
+const command = process.argv[2];
 
-console.log(msg);
+// ...and manage them
+if(command === 'add') console.log('Adding note!');
+if(command === 'remove') console.log('Removing note!');
--- notes.js
+++ notes.js
@@ -1,3 +1,3 @@
-const getNotes = () => 'Your Notes';
+const getNotes = () => 'Success!';
 
 module.exports = getNotes;
